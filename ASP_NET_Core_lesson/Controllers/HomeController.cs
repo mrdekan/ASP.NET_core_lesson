f0ffc27c3bd9c95bd532b0309aa3306cc3348ccf
mrdekan <yeros05040504@gmail.com>
2023-02-17 19:34:16 +0200

    Edited

--- ASP_NET_Core_lesson/Controllers/HomeController.cs
+++ ASP_NET_Core_lesson/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace ASP_NET_Core_lesson.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
