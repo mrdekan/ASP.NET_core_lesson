aaaff8435fa72130551e5ce5087e778b77700597
mrdekan <yeros05040504@gmail.com>
2023-02-17 20:23:29 +0200

    Edited

---
+++ ASP_NET_Core_lesson/Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Core_lesson.Controllers
+{
+	public class ContactsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ ASP_NET_Core_lesson/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace ASP_NET_Core_lesson.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+    }
+}
